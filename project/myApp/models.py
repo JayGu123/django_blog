266fa0b04119e042b330a2e2e61fd1db42aafe93
Jay Gu <2512855228@qq.com>
2018-03-09 16:52:30 +0800

    2018-3-9

--- project/myApp/models.py
+++ project/myApp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+class POST(models.Model):
+    author = models.ForeignKey(User, on_delete='descase')
+    title = models.CharField(max_length=20)
+    text = models.TextField()
+    createdtime = models.DateTimeField(default=timezone.now)
+    publishedtime = models.DateTimeField(blank=True, null=True)
+
+
+    def publish(self):
+        self.published_time = timezone.now
+        self.save()
+
+    def __str__(self):
+        return self.title
--- project/myApp/views.py
+++ project/myApp/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import POST
 # Create your views here.
+
+def hello(request):
+    posts = POST.objects.all()
+    return render(request, 'myApp/hello.html',{'post' :posts})
